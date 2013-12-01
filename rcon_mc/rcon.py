10d9f1ca9af74fb248fc06ab22596ba40e8fbe2a
paws <paws@delimitize.com>
2013-12-01 03:25:15 -0800

    - added unpack logic in Rcon.receive function for matching messages and   determining if more data could be incoming

--- rcon_mc/rcon.py
+++ rcon_mc/rcon.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-''' rcon.py - RCON protocol interface module
+'''
+ rcon.py - RCON protocol interface module
  Author:
    paws
  Project Repo:
@@ -11,20 +12,10 @@
 import struct
 import socket
 
-
-'''
-TCP Packet Structure
-Field  Type  Value    Python_Representation
-Size   32-bit little-endian Signed Integer  'i<'  
-ID   32-bit little-endian Signed Integer   '<i'
-Type   32-bit little-endian Signed Integer   '<i' 
-Body  Null-terminated ASCII String   Varies "string\x00"
-Empty String  Null-terminated ASCII String   0x00 '\x00'
-'''
-
 '''Module Declarations:'''
 NULL='\x00' ## hex for C null terminator
 MIN_PACKET_SIZE=10  # 4(id)+4(type)+1(body)+1(empty string)
+MIN_PACKET_ACTUAL_SIZE=14 # 4(size) + 4(id)+4(type)+1(body)+1(empty string)
 MAX_PACKET_SIZE=4096 # just the way it is
 MAX_BODY_SIZE=4086 # (4096 - 4(id) - 4(type) - 1(empty string) -1(body null-terminator)) = 4086
 
@@ -48,7 +39,7 @@ class RconArgumentException(RconException):
   '''For passing RCON argument'''
   pass
 
-class RconProgrammingError(RconException):
+class RconProgramException(RconException):
   '''For passing errors in logic RCON  (debug)'''
   pass
 
@@ -71,7 +62,6 @@ class Rcon:
       self.connection.close()
     self.connection=None
     self.error_stack.append(ret_val)
-
 
   def connect(self):
     '''Resolve remote host and connect however possible (IPV6 compat)'''
@@ -129,13 +119,35 @@ class Rcon:
      feels like I should be able to do this in one pack call? i.e. '<i<i<i', but works
       packet = struct.pack('<i', size) + struct.pack('<i', self.id) + struct.pack('<i', type) +  msg + NULL + NULL
     '''
-    packet = struct.pack('<i', size) + struct.pack('<i', self.id) + struct.pack('<i', type) +  msg + NULL + NULL
-    self.connection.send(packet)
+    try: 
+      packet = struct.pack('<i', size) + struct.pack('<i', self.id) + struct.pack('<i', type) +  msg + NULL + NULL
+    except(struct.error) as ret_val:
+      raise RconProgramExceptio("Unable to create TCP packet: " + str(error_stack))
+      return False
+    try: 
+      self.connection.send(packet)
+    except(socket.error) as ret_val:
+      raise RconSocketException("Send failure: " + str(ret_val))
+      return False
+    return True
 
   def receive(self):
-    return self.connection.recv(4096)
+    '''Read responses from the socket and unpack them'''
+    unpack_fmt="<iiixx" ## empty body packet format
+    try:
+      packet = self.connection.recv(MAX_PACKET_SIZE)
+    except(socket.error) as ret_val:
+      self._manage_socket_error(ret_val)
+      raise RconSocketException("Failed to receive data from socket:" + str(self.error_stack))
+      return False
+    msg_size = len(packet) - MIN_PACKET_ACTUAL_SIZE 
+    if msg_size > 0:
+      unpack_fmt = "<iii" + str(msg_size) + "sxx"
+    payload = struct.unpack(unpack_fmt, packet)
+    return payload
 
   def rcon(self, command):
+    '''Higher level function that manages message id matching'''
     self.send(3,self.password)
     self.receive()
     self.send(2,command)
