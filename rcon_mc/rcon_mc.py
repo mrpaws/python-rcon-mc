1c67073c4e5db51e4324ce462396371ea69b316c
paws <paws@delimitize.com>
2013-12-10 03:51:54 -0800

    - continuing work on direct MC rcon interface, new branch

--- rcon_mc/rcon_mc.py
+++ rcon_mc/rcon_mc.py
@@ -16,13 +16,4 @@ class RconMCException(Exception):
 
 class client(rcon):
   '''class to execute commands on the server'''
-  '''
-  def __init__(self, host, port, password ):
-    self.host=host
-    self.port=port
-    self.password=password
-    self.error_stack=[]
-    assert type(self.host) is StringType, "{m}{h}".format(m="hostname is not a string:", h=self.host)
-    assert type(self.port) is IntType, "{m}{p}".format(m="port is not a number:", p=self.port)
-    assert type(self.host) is StringType, "password is not a string"
-  '''
+
